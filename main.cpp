2affec40dbb92c0cdfb87f90818b984591dbaecd
-=Oktan=- <r.zhigalenko@mail.ru>
2022-01-30 01:16:44 +0500

    add local map memory

--- main.cpp
+++ main.cpp
@@ -1,28 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <map>
+#include <new>
 #include "myalocator.h"
 #include "unlist.h"
 
 int main(int, char**)
 {
   
-    // otus::unlist<int, Myallocator<int>> list;
+      std::vector<std::string, Myallocator<std::string>> list;
+        for(std::size_t i =0; i < 5; ++i)
+         list.push_back("i");
+    std::cout << list[0] << std::endl;
+   
 
-    // for(std::size_t i =0; i < 50000; ++i)
-    //     list.push_front(i);
+//    //Сделать резервирование памяти в самом алокаторе и уже отуда отдавать ее!!!
+
+    // std::vector<int, Myallocator<int>> vec{10, 10, 10};
+  
+//    auto m = std::map<
+// 		int,
+// 		int,
+// 		std::less<int>,
+// 		Myallocator<
+// 			std::pair<
+// 				const int, int>>>{};
+
+// 	for (int i = 0; i < 5; ++i) {
+// 		m[i] = i;
+// 	}
+
+//     for(auto it = m.begin(); it != m.end(); ++it)
+//         std::cout<< it->second << std::endl;
+
     
-
-
-   //Сделать резервирование памяти в самом алокаторе и уже отуда отдавать ее!!!
-
-    std::vector<int, Myallocator<int>> vec;
-    //vec.reserve(10);
-    std::cout << "reserv: 10 \n";
-
-    for(int i = 0; i < 10; ++i)
-    {
-        vec.emplace_back(i);
-        std::cout<< i << std::endl;
-    }
     return 0;
 }
--- myalocator.h
+++ myalocator.h
@@ -2,55 +2,109 @@
 #include <memory>
 #include <limits>
 
-template <class T>
+template <class T, int s_byte = 500>
 struct Myallocator
-{
+{     
   using value_type = T;
- 
-  Myallocator () = default;
-  template <class U> 
-  constexpr Myallocator (const Myallocator <U>&) noexcept {}
- 
-  T* allocate(std::size_t n) {
-    if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
-      throw std::bad_array_new_length();
- 
-    if (auto p = static_cast<T*>(std::malloc(n*sizeof(T)))) {
-      report(p, n);
-      return p;
-    }
- 
-    throw std::bad_alloc();
-  }
- 
-  void deallocate(T* p, std::size_t n) noexcept {
-    report(p, n, 0);
-    std::free(p);
+  using pointer = T*;
+  using const_pointer = const T*;
+  using reference = T&;
+  using const_reference = const T&;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+  using propagate_on_container_move_assignment = std::true_type;
+  using is_always_equal = std::true_type;
+
+private:
+  
+  char buff[s_byte];
+  size_type _size = 0;
+  char* begin = nullptr;
+  char* current = nullptr;
+  char* end = nullptr;
+
+public:
+
+  template <typename U>
+  struct rebind
+  {
+    using other = Myallocator<U>;
+  };
+
+  Myallocator()
+  {
+    begin = buff;
+    _size = sizeof(buff) / sizeof(value_type);
+    end = begin + sizeof(buff);
+    current = begin;
   }
 
-  template< class U, class... Args >
-  void construct( U* p, Args&&... args )
+  template <class U>
+  constexpr Myallocator(const Myallocator<U> &) noexcept {}
+
+  pointer address(reference x) const 
   {
-    //report(p, 0, 0);
-    printf("args: %d \n", args ...);
-      new((void *)p) U(std::forward<Args>(args)...);
-      
+    return &x; 
   }
- 
+  const_pointer address(const_reference x) const 
+  {
+    return &x; 
+  }
+
+  pointer allocate(size_type n)
+  {
+    
+    auto p = reinterpret_cast<pointer>(current);
+    current +=sizeof(value_type)*n;
+    report(p, n);
+         
+    return p;
+    // if (n > std::numeric_limits<std::size_t>::max() / sizeof(T))
+    //   throw std::bad_array_new_length();
+
+    // if (auto p = static_cast<T *>(std::malloc(n * sizeof(T))))
+    // {
+    //   report(p, n);
+    //   return p;
+    // }
+
+    // throw std::bad_alloc();
+  }
+
+  void deallocate(pointer p, size_type n) noexcept
+  {
+    report(p, n, 0);
+    p = nullptr;
+    // std::free(p);
+  }
+
+  template <class U, class... Args>
+  void construct(U *p, Args &&...args)
+  {
+    std::cout << "construct\n";
+    //  printf("args: %d \n", args ...);
+    new ((void *)p) U(std::forward<Args>(args)...);
+  }
+
+  template< class U >
+  void destroy( U *p )
+  {
+    std::cout << "destoy\n";
+    p->~U();
+  }
+
 private:
-  void report(T* p, std::size_t n, bool alloc = true) const {
-    std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(T)*n
-      << " bytes at " << std::hex << std::showbase
-      << reinterpret_cast<void*>(p) << std::dec << '\n';
+  
+
+  void report(T *p, std::size_t n, bool alloc = true) const
+  {
+    std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(T) * n
+              << " bytes at " << std::hex << std::showbase
+              << reinterpret_cast<void *>(p) << std::dec << '\n';
   }
 };
- 
+
 template <class T, class U>
-bool operator==(const Myallocator <T>&, const Myallocator <U>&) { return true; }
+bool operator==(const Myallocator<T> &, const Myallocator<U> &) { return true; }
 template <class T, class U>
-bool operator!=(const Myallocator <T>&, const Myallocator <U>&) { return false; }
-
-
-
-
-
+bool operator!=(const Myallocator<T> &, const Myallocator<U> &) { return false; }
