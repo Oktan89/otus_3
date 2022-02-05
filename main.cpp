7aa24f1cb65da942369de8f023a945c862ca1904
-=Oktan=- <r.zhigalenko@mail.ru>
2022-02-05 13:42:38 +0500

    fix allocator alloc mam erase

--- main.cpp
+++ main.cpp
@@ -1,34 +1,32 @@
 #include <iostream>
 #include <vector>
 #include <map>
-#include <new>
-#include <string>
-#include <forward_list>
-#include <algorithm>
+#include <list>
 #include "myalocator.h"
 #include "unlist.h"
+#include "timer.h"
 
-class my
-{
-    int b;
-    friend std::ostream& operator<<(std::ostream& out, const my& m);
-public:
-    my(){}
-    my(int a) : b(a)
-    {
-        std::cout<<"construct my "<<b <<"\n";
-    }
-    ~my()
-    {
-        std::cout<<"destruct ~my "<<b<<"\n";
-    }
-};
-
-std::ostream& operator<<(std::ostream& out, const my& m)
-{
-    out << m.b;
-    return out;
-}
+// class my
+// {
+//     int b;
+//     friend std::ostream& operator<<(std::ostream& out, const my& m);
+// public:
+//     my(){}
+//     my(int a) : b(a)
+//     {
+//         // std::cout<<"construct my "<<b <<"\n";
+//     }
+//     ~my()
+//     {
+//         // std::cout<<"destruct ~my "<<b<<"\n";
+//     }
+// };
+// 
+// std::ostream& operator<<(std::ostream& out, const my& m)
+// {
+//     out << m.b;
+//     return out;
+// }
 
 std::size_t fact(std::size_t n)
 {
@@ -42,40 +40,56 @@ std::size_t fact(std::size_t n)
 int main(int, char**)
 {  
 
-    std::map<int, int> standart;
-    std::map<int, int, std::less<int>, Myallocator<int, 10>> myalloc;
+    // std::map<int, int> standart;
+    // std::map<int, int, std::less<int>, otus::Myallocator<int, 10>> myalloc;
+   
+    // for(std::size_t i = 0; i < 10; ++i)
+    // {
+    //     std::size_t f = fact(i);
+    //     standart[i] = f;
+    //     myalloc[i] = f;
+    // }
+  
+    // std::cout<< "\nMAP std::alocator\n";
+    // for(auto& [key, set] : standart)
+    //     std::cout<< key << " " << set << std::endl;
+    
+    // std::cout<< "\nMAP Myalocator\n";
+    // for(auto& [key, set] : standart)
+    //     std::cout<< key << " " << set << std::endl;
 
-    for(std::size_t i = 0; i < 10; ++i)
-    {
-        std::size_t f = fact(i);
-        standart[i] = f;
-        myalloc[i] = f;
-    }
+    // otus::unlist<int> myl;
+    // otus::unlist<int, otus::Myallocator<int, 10>> mylistalloc;
 
-    std::cout<< "\nMAP std::alocator\n";
-    for(auto& [key, set] : standart)
-        std::cout<< key << " " << set << std::endl;
+    // for(std::size_t i = 0; i < 10; ++i)
+    // {
+    //     myl.push_back(i);
+    //     mylistalloc.push_back(i);
+    // }
     
-    std::cout<< "\nMAP Myalocator\n";
-    for(auto& [key, set] : standart)
-        std::cout<< key << " " << set << std::endl;
-    
-    
-    otus::unlist<int> my;
-    otus::unlist<int, Myallocator<int, 10>> mylistalloc;
-    for(std::size_t i = 0; i < 10; ++i)
-    {
-        my.push_back(i);
-        mylistalloc.push_back(i);
-    }
-    
-    std::cout<< "\nMy conteiner std::alocator\n";
-    for(auto& m: my)
-        std::cout<< m <<std::endl;
+    // std::cout<< "\nMy conteiner std::alocator\n";
+    // for(auto& m: myl)
+    //     std::cout<< m <<" ";
+    // std::cout<<std::endl;
 
-    std::cout<< "\nMy conteiner Myalocator\n";
-    for(auto it = mylistalloc.begin(); it != mylistalloc.end(); ++it)
-        std::cout<< *it <<std::endl;
+    // std::cout<< "\nMy conteiner Myalocator\n";
+    // for(auto it = mylistalloc.begin(); it != mylistalloc.end(); ++it)
+    //     std::cout<< *it <<" ";
+    // std::cout<<std::endl;
+ 
+
+    std::list<int, otus::Myallocator<int, 6>> vecm;
+
+    for(std::size_t i = 0; i < 5; ++i)
+        vecm.push_front(i);
     
+    auto it = vecm.begin();
+    vecm.erase(it);
+    vecm.push_front(12);
+    vecm.push_front(10);
+
+    for(auto& s : vecm)
+        std::cout<< s << " ";
+    std::cout<<std::endl;
     return 0;
 }
--- myalocator.h
+++ myalocator.h
@@ -4,147 +4,193 @@
 #include <limits>
 #include <cassert>
 
-#define _LOG_ false
+#define _LOG_ 1
 
 //1 param T - object
 //2 param set_size - sets the number of objects to be placed on the stack
-template <class T, int set_size = 500>
-class Myallocator
+namespace otus
 {
-public:     
-  using value_type = T;
-  using pointer = T*;
-  using const_pointer = const T*;
-  using reference = T&;
-  using const_reference = const T&;
-  using size_type = std::size_t;
-  using difference_type = std::ptrdiff_t;
-  using propagate_on_container_move_assignment = std::true_type;
-  using is_always_equal = std::true_type;
-  
-private:
- 
-  char buff[set_size * sizeof(T)];
-  char buff_2[sizeof(buff)];
 
-  size_type _max_size = sizeof(buff) / sizeof(value_type);
-  size_type _courent_obg_T = 0;
-  char* begin = buff;
-  char* current = begin;
-  char* end = begin + sizeof(buff);
-
-  char* begin_2 = buff_2;
-  char* current_2 = begin_2;
-  char* end_2 = begin_2 + sizeof(buff_2);
-
-  bool switch_mem = true;
-public:
-
-  template <typename U, int copy_byte = set_size>
-  struct rebind
+  template <class T, int set_size = 500>
+  class Myallocator
   {
-    using other = Myallocator<U, copy_byte>;
+  public:     
+    using value_type = T;
+    using pointer = T*;
+    using const_pointer = const T*;
+    using reference = T&;
+    using const_reference = const T&;
+    using size_type = std::size_t;
+    using difference_type = std::ptrdiff_t;
+    using propagate_on_container_move_assignment = std::true_type;
+    using is_always_equal = std::true_type;
+    
+  private:
+  
+    char buff[set_size * sizeof(T)];
+    char buff_2[sizeof(buff)];
+
+    size_type _max_size = sizeof(buff) / sizeof(value_type);
+    size_type _courent_obg_T = 0;
+    char* begin = buff;
+    char* current = begin;
+    char* end = begin + sizeof(buff);
+
+    char* begin_2 = buff_2;
+    char* current_2 = begin_2;
+    char* end_2 = begin_2 + sizeof(buff_2);
+
+    bool switch_mem = true;
+  public:
+
+    template <typename U, int copy_byte = set_size>
+    struct rebind
+    {
+      using other = Myallocator<U, copy_byte>;
+    };
+
+    Myallocator() {initmem();}
+    ~Myallocator() = default;
+
+    template <class U, int copy_byte = set_size>
+    constexpr Myallocator(const Myallocator<U, copy_byte> &) noexcept {}
+
+    pointer address(reference x) const 
+    {
+      return &x; 
+    }
+    const_pointer address(const_reference x) const 
+    {
+      return &x; 
+    }
+
+    pointer allocate(size_type n)
+    {
+      if(n > _max_size || _courent_obg_T >=_max_size)
+        throw std::runtime_error("bad_allocate memory for object_" + std::to_string(_courent_obg_T+1) +". Allocated size " 
+          + std::to_string(_courent_obg_T) +", max size: " + std::to_string(_max_size)  + 
+          " \'try increasing the set_size parameter allocator \'");
+  
+      pointer p;
+      if(switch_mem)
+      {
+          p = reinterpret_cast<pointer>(current);
+          p = setmem(p, end, n);
+          if(p == nullptr)
+            throw std::bad_alloc();
+          current +=sizeof(value_type)*n;
+          switch_mem = false;
+      }
+      else
+      {
+          p = reinterpret_cast<pointer>(current_2);
+          p = setmem(p, end_2, n);
+          if(p == nullptr)
+            throw std::bad_alloc();
+          current_2 +=sizeof(value_type)*n;
+          switch_mem = true;
+      }
+  #if _LOG_
+      report(p, n);
+  #endif
+      if(current_2 > end_2 || current > end)
+        throw std::bad_alloc();
+      _courent_obg_T += n;    
+      return p;
+
+    }
+private:
+    void initmem() noexcept
+    {
+      for(std::size_t i = 0; i < sizeof(buff); ++i)
+      {
+        buff[i] = '\n';
+        buff_2[i] = '\n';
+      }
+    }
+    pointer setmemObj(pointer p,size_type n)
+    {
+        char *__p = reinterpret_cast<char *>(p);
+        bool memused = false;
+        for (std::size_t i = 0; i < n * sizeof(value_type); ++i)
+        {
+          if (*__p != '\n')
+          {
+            memused = true;
+            break;
+          }
+          ++__p;
+        }
+      return (memused)? nullptr : p;
+    }
+
+    pointer setmem(pointer p, char* end, size_type n)
+    {
+        while(reinterpret_cast<char *>(p) < end)
+        {
+          if(nullptr == setmemObj(p, n))
+            ++p;
+          else return p;
+        }
+        return nullptr;
+    }
+public:
+    void deallocate([[maybe_unused]] pointer p, [[maybe_unused]] size_type n) noexcept
+    {
+  #if _LOG_
+      report(p, n, 0);
+  #endif
+      [[maybe_unused]] char* __p = reinterpret_cast<char*>(p);
+      for(std::size_t i =0; i < n * sizeof(value_type); ++i)
+      {
+          [[maybe_unused]] char* temp = __p;
+          __p++;
+          *temp = '\n';     
+      }
+      
+      (switch_mem)? current = begin : current_2 = begin_2;
+      _courent_obg_T -= n;
+    }
+
+    template <class U, class... Args>
+    void construct(U *p, Args &&...args)
+    {
+  #if _LOG_
+      std::cout << "construct: " << typeid(p).name() << '\n';
+  #endif
+      new ((void *)p) U(std::forward<Args>(args)...);
+
+    }
+
+    template< class U >
+    void destroy( U *p )
+    {
+  #if _LOG_
+      std::cout << "destroy: " << typeid(p).name() << '\n';
+  #endif
+      p->~U();
+    }
+
+    value_type max_size()
+    {
+      return _max_size;
+    }
+
+  private:
+    
+  #if _LOG_
+    void report(T *p, std::size_t n, bool alloc = true) const
+    {
+      std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(T) * n
+                << " bytes at " << std::hex << std::showbase
+                << reinterpret_cast<void *>(p) << std::dec << '\n';
+    }
+  #endif
   };
 
-  Myallocator() = default;
-  ~Myallocator() = default;
+  template <class T, class U>
+  bool operator==(const Myallocator<T> &, const Myallocator<U> &) { return true; }
+  template <class T, class U>
+  bool operator!=(const Myallocator<T> &, const Myallocator<U> &) { return false; }
 
-  template <class U, int copy_byte = set_size>
-  constexpr Myallocator(const Myallocator<U, copy_byte> &) noexcept {}
-
-  pointer address(reference x) const 
-  {
-    return &x; 
-  }
-  const_pointer address(const_reference x) const 
-  {
-    return &x; 
-  }
-
-  pointer allocate(size_type n)
-  {
-    if(n > _max_size || _courent_obg_T >=_max_size)
-      throw std::runtime_error("bad_allocate memory for object_" + std::to_string(_courent_obg_T+1) +". Allocated size " 
-        + std::to_string(_courent_obg_T) +", max size: " + std::to_string(_max_size)  + 
-        " \'try increasing the set_size parameter allocator \'");
- 
-    pointer p;
-    if(switch_mem)
-    {
-        p = reinterpret_cast<pointer>(current);
-        current +=sizeof(value_type)*n;
-        switch_mem = false;
-    }
-    else
-    {
-        p = reinterpret_cast<pointer>(current_2);
-        current_2 +=sizeof(value_type)*n;
-        switch_mem = true;
-    }
-#if _LOG_
-    report(p, n);
-#endif
-    if(current_2 > end_2 || current > end)
-      throw std::bad_alloc();
-    _courent_obg_T += n;    
-    return p;
-
-  }
-
-  void deallocate([[maybe_unused]] pointer p, [[maybe_unused]] size_type n) noexcept
-  {
-#if _LOG_
-    report(p, n, 0);
-#endif
-    [[maybe_unused]] char* __p = reinterpret_cast<char*>(p);
-    for(std::size_t i =0; i < n * sizeof(value_type); ++i)
-    {
-         [[maybe_unused]] char* temp = __p;
-         __p++;
-         *temp = '\000';     
-    }
-    
-    (switch_mem)? current = begin : current_2 = begin_2;
-    _courent_obg_T -= n;
-  }
-
-  template <class U, class... Args>
-  void construct(U *p, Args &&...args)
-  {
-#if _LOG_
-    std::cout << "construct: " << typeid(p).name() << '\n';
-#endif
-    new ((void *)p) U(std::forward<Args>(args)...);
-
-  }
-
-  template< class U >
-  void destroy( U *p )
-  {
-#if _LOG_
-    std::cout << "destroy: " << typeid(p).name() << '\n';
-#endif
-    p->~U();
-  }
-
-  value_type max_size()
-  {
-    return _max_size;
-  }
-
-private:
-  
-#if _LOG_
-  void report(T *p, std::size_t n, bool alloc = true) const
-  {
-    std::cout << (alloc ? "Alloc: " : "Dealloc: ") << sizeof(T) * n
-              << " bytes at " << std::hex << std::showbase
-              << reinterpret_cast<void *>(p) << std::dec << '\n';
-  }
-#endif
-};
-
-template <class T, class U>
-bool operator==(const Myallocator<T> &, const Myallocator<U> &) { return true; }
-template <class T, class U>
-bool operator!=(const Myallocator<T> &, const Myallocator<U> &) { return false; }
+}
